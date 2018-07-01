dafda5e1a5a514a00f71051a257f3bd36038eff9
chengshiyu <chengshiyu0605@163.com>
2018-07-01 23:13:09 +0800

    '0701'

--- Javascript/Spider/RequestCheerio.js
+++ Javascript/Spider/RequestCheerio.js
@@ -10,9 +10,10 @@ request({
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36'
   }
 }, function (error, response, body) {
-  if (!error) {
-    const $ = cheerio.load(body);
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(body, {
+      ignoreWhitespace: true
+    });
     console.log($.html());
-    // console.log(body)
   }
 });
